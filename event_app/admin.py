6cde457a52c773cdbd2feac901236da0d1411270
Aoki <aoki.h.24kdgn@gmail.com>
2025-06-10 07:41:50 +0900

    admin_custumusermodel

--- event_app/admin.py
+++ event_app/admin.py
@@ -1,3 +1,23 @@
+#カスタムユーザーモデルをadmin管理画面でも表示される
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('アクセス権', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('その他', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
